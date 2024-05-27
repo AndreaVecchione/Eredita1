992391e8a056882e873dc15b92696184ec20ee6d
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-05-27 18:56:35 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Gatto gatto = new Gatto();
         Animale animale = new Animale();
+        //Richiamo il metodo della classe Animale e della sua sottoclasse
        animale.faiIlVerso();
        gatto.faiIlVerso();
     }
 
 }
-
 
 
 
