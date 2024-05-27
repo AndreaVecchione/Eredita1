36e0ec423b0f7cd82b8e652a7d7e7d8a992ee734
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-05-27 18:57:17 +0200

    Animale.java

--- src/Animale.java
+++ src/Animale.java
@@ -1,5 +1,7 @@
+//Creo la classe Animale
 public class Animale {
 
+//Metodo che andrà a stamparmi il verso dell'animale
     public void faiIlVerso() {
         System.out.println("L'animale fà il verso");
     }
